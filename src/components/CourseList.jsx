e7cfb8efa3cd284dc8cb499e4639a44b483d39be
Dawid <105428376+Dawka86@users.noreply.github.com>
2026-01-21 13:25:32 +0100

    update

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -2,28 +2,28 @@ import { PiCertificateLight } from "react-icons/pi";
 
 const courses = [
   {
-    title: `"The Complete Full-Stack Web Development Bootcamp"`,
+    title: "The Complete Full-Stack Web Development Bootcamp",
     institution: "",
     paltforma: "Udemy",
     link: "https://www.udemy.com/certificate/UC-a961e4dd-56c4-4dff-b493-e4e675f369ef/",
   },
   {
-    title: `"Website Design"`,
+    title: "Website Design",
     institution: "Akademia Ekonomiczno-Humanistyczna w Warszawie",
     paltforma: "Navoica",
     link: "https://navoica.pl/certificates/eb3c31bdc5ad4ff0a846ead8f71932a7",
   },
   {
-    title: `"JavaScript Programming Basics"`,
+    title: "JavaScript Programming Basics",
     institution: "",
     paltforma: "Navoica",
     link: "https://navoica.pl/certificates/9b6aa671a332408fa463434f653d654a",
   },
   {
-    title: `"Angular - The Complete Guide"`,
+    title: "Angular - The Complete Guide - Currently learning",
     institution: "",
     paltforma: "Udemy",
-    link: "Currently learning",
+    link: "", 
   },
 ];
 
@@ -37,13 +37,16 @@ export default function CourseList() {
             className="list-group-item d-flex justify-content-between align-items-start py-2"
           >
             <div className="ms-2 me-auto">
-              <strong>{course.title}</strong> – {course.institution} (
-              {course.paltforma})
+              <strong>{course.title}</strong> – {course.institution} ({course.paltforma})
               <br />
               <PiCertificateLight style={{ fontSize: "25px" }} />{" "}
-              <a href={course.link} target="_blank" rel="noopener noreferrer">
-                Certificate
-              </a>
+              {course.link ? (
+                <a href={course.link} target="_blank" rel="noopener noreferrer">
+                  Certificate
+                </a>
+              ) : (
+                <span style={{ fontStyle: "italic", color: "gray" }}>Currently learning</span>
+              )}
             </div>
           </li>
         ))}
